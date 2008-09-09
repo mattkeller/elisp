613735265eda54c2f26955d46f7ac2744cbe885f
Matt Keller <mk@littleredbat.net>
2008-09-09 14:48:23 -0400

    First working version of project helpers in mk-project.el

---
+++ mk-project.el
@@ -0,0 +1,151 @@
+;;;; mk-project.el --  Lightweight 'project's
+
+;;;; Define new wrapper fns (grep, tags, etc) that use new vars to customize
+;;;; their operations. Don't touch std emacs vars or fns. On project init, 
+;;;; set vars, set keybindings.
+;;;;
+;;;; Operations: 
+;;;;   Set tags file
+;;;;   Rebuild tags file
+;;;;   Grep the project properly
+;;;;   Open shell in project base dir
+;;;;   Build project
+;;;;   Complile file in project
+;;;;   VC operations?
+;;;;   Provide hooks for further per-project setup
+
+;; ---------------------------------------------------------------------
+;; Projects: defined ${project-name}-config vars for each project
+;; ---------------------------------------------------------------------
+
+(defvar qrev-config '((name "qrev")
+                      (basedir "/home/mk/code/lisp/qrev/")
+                      (src-patterns "*.lisp")
+                      (ignore-patterns "*.fasl")
+                      (tags-file "/home/mk/code/lisp/qrev/TAGS")
+                      (git-p t)
+                      (compile-cmd "make -k")
+                      (startup-hook qrev-startup-hook)
+                      (shutdown-hook qrev-shutdown-hook)))
+
+(defun qrev-startup-hook () (message "Qrev it up, baby!"))
+
+(defun qrev-shutdown-hook () (message "Adios, qrev"))
+
+;; ---------------------------------------------------------------------
+;; Utils
+;; ---------------------------------------------------------------------
+
+(defmacro aif (test true-body &rest false-body)
+  "Evaluate TRUE-BODY or FALSE-BODY depending on value of TEST.
+If TEST returns non-nil, bind `it' to the value, and evaluate
+TRUE-BODY.  Otherwise, evaluate forms in FALSE-BODY as if in `progn'.
+Compare with `if'."
+  (let ((sym (gensym "--ibuffer-aif-")))
+    `(let ((,sym ,test))
+       (if ,sym
+	   (let ((it ,sym))
+	     ,true-body)
+	 (progn
+	   ,@false-body)))))
+
+;; ---------------------------------------------------------------------
+;; Setup fns
+;; ---------------------------------------------------------------------
+
+(defun project-defaults ()
+  (setq mk-proj-name nil
+        mk-proj-basedir (getenv "HOME")
+        mk-proj-src-patterns nil
+        mk-proj-ignore-patterns nil
+        mk-proj-git-p nil
+        mk-proj-tags-file nil
+        mk-proj-compile-cmd "make -k"
+        mk-proj-startup-hooks nil
+        mk-proj-shutdown-hooks nil)
+  (cd mk-proj-basedir)
+  (global-set-key [f5] 'compile)
+  (global-set-key [f6] 'grep-find)
+  (global-unset-key (kbd "C-c t")))
+
+(defun config-val (key config-alist)
+  (if (assoc key config-alist)
+    (car (cdr (assoc key config-alist)))
+    nil))
+
+(defun project-load-vars (proj-alist)
+  (project-defaults)
+  ;; required vars
+  (setq mk-proj-name (config-val 'name proj-alist))
+  (setq mk-proj-basedir (config-val 'basedir proj-alist))
+  ;; optional vars
+  (aif (config-val 'src-patterns proj-alist) (setq mk-proj-src-patterns it))
+  (aif (config-val 'ignore-patterns proj-alist) (setq mk-proj-ignore-patterns it))
+  (aif (config-val 'git-p proj-alist) (setq mk-proj-git-p it))
+  (aif (config-val 'tags-file proj-alist) (setq mk-proj-tags-file it))
+  (aif (config-val 'compile-cmd proj-alist) (setq mk-proj-compile-cmd it))
+  (aif (config-val 'startup-hook proj-alist) (setq mk-proj-startup-hooks (list it)))
+  (aif (config-val 'shutdown-hook proj-alist) (setq mk-proj-shutdown-hooks (list it))))
+
+(defun project-start (name)
+  (interactive "sProject Alist Name: ")
+  (project-load-vars (symbol-value (intern-soft (concat name "-config")))) ; TODO: improve error handling!
+  (cd mk-proj-basedir)
+  (when mk-proj-tags-file (visit-tags-table mk-proj-tags-file))
+  (global-set-key [f5] 'project-compile)
+  (global-set-key [f6] 'project-grep)
+  (global-set-key (kbd "C-c t") 'project-tags-build)
+  (when mk-proj-startup-hooks
+    (run-hooks 'mk-proj-startup-hooks)))
+
+(defun project-quit ()
+  (interactive)
+  (when mk-proj-shutdown-hooks
+    (run-hooks 'mk-proj-shutdown-hooks))
+  (project-defaults)
+  (message "Project settings have been cleared"))
+
+(defun project-status ()
+  (interactive)
+  (message "Name=%s; Basedir=%s; Src=%s; Ignore=%s; Git-p=%s; Tags=%s; Compile=%s; Startup=%s; Shutdown=%s"
+           mk-proj-name mk-proj-basedir mk-proj-src-patterns mk-proj-ignore-patterns mk-proj-git-p 
+           mk-proj-tags-file mk-proj-compile-cmd mk-proj-startup-hooks mk-proj-shutdown-hooks))
+
+;; ---------------------------------------------------------------------
+;; Operational fns
+;; ---------------------------------------------------------------------
+
+(defun project-tags-build ()
+  (interactive)
+  (if mk-proj-tags-file
+    (progn
+      (cd mk-proj-basedir)
+      (message "Refreshing TAGS file %s..." mk-proj-tags-file)
+      (call-process-shell-command (concat "etags `find " mk-proj-basedir " -type f` -o " mk-proj-tags-file))
+      (message "Done refreshing TAGS file %s." mk-proj-tags-file)
+      (visit-tags-table mk-proj-tags-file))
+    (message "mk-proj-tags-file is not set")))
+  
+(defun project-grep (s)
+  (interactive "sGrep project for: ")
+  (cd mk-proj-basedir)
+  (let ((find-cmd (concat "find . -type f"))
+        (grep-cmd (concat "grep -n \"" s "\"")))
+    (when mk-proj-src-patterns
+      (setq find-cmd (concat find-cmd " -name \"" mk-proj-src-patterns "\"")))
+    (when mk-proj-tags-file
+      (setq find-cmd (concat find-cmd " -not -name 'TAGS'")))
+    (when mk-proj-git-p 
+      (setq find-cmd (concat find-cmd " -not -path '*/.git*'")))
+    (grep-find (concat find-cmd " -print0 | xargs -0 -e " grep-cmd))))
+
+(defun project-compile (opts)
+  (interactive "sCompile options: ")
+  (cd mk-proj-basedir)
+  (compile (concat mk-proj-compile-cmd opts))) 
+
+;; ---------------------------------------------------------------------
+;; Run me!
+;; ---------------------------------------------------------------------
+
+(project-defaults)
