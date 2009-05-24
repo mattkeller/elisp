605970b2054cc142f80d279b2e17618158797226
Matt Keller <mk@littleredbat.net>
2009-05-24 13:25:18 -0400

    Enable jde-compile, jde-ant-build for mcp projects
    * Parse all of viewdir, workdir, stagedir from proj file
    * Setup new JDE variables
    * Move basic JDE setup (jdk-registry) out of hook, into dotemacs
    * Don't defer loading JDE at startup

--- mcp-projects.el
+++ mcp-projects.el
@@ -12,6 +12,14 @@
 (defun mktop-p ()
   (string-equal system-name "mktop"))
 
+(defun normalize-dirname (dir)
+  "Ensure dir ends in '/'"
+  (let* ((len (length dir))
+         (last-char (substring dir (- len 1) len)))
+    (if (string-equal last-char "/")
+        dir
+      (concat dir "/"))))
+
 ;;; --------------------------------------------------------------------
 ;;; MCP JDE utilities
 ;;; --------------------------------------------------------------------
@@ -33,20 +41,13 @@
                              "masSoapServices/masservice.jar"
                              "jazzlib/jazzlib.jar")))
 
-(defvar mcp-jde-preferred-jdk (if (mktop-p) "1.6.0_13" "1.6.0_11")
-  "The preferred mcp12 jdk to use per machine")
 
-(defvar mcp-jde-jdk-registry (if (mktop-p)
-                        '(("1.6.0_13" . "/opt/jdk1.6.0_13"))
-                      '(("1.5.0_11" . "/localdisk/jdk1.5.0_11")
-                        ("1.6.0_05" . "/localdisk/jdk1.6.0_05")
-                        ("1.6.0_11" . "/localdisk/data/matthewk/local/jdk1.6.0_11"))))
 
 (defun mcp-jde-jars-from-build-xml (basedir)
   "Return the list of 'standard.jars' from build.xml"
   (let* ((jars nil)
          (root (with-temp-buffer
-                 (insert-file-contents (concat basedir "/mcp_core_root/loadbuild/scripts/build.xml"))
+                 (insert-file-contents (concat basedir "mcp_core_root/loadbuild/scripts/build.xml"))
                  (xml-parse-region (point-min) (point-max))))
          (project (car root)))
     (dolist (c (xml-get-children project 'path))
@@ -56,23 +57,47 @@
             (dolist (elem (xml-get-children c 'pathelement))
               (let ((jar (cdr (assq 'location (xml-node-attributes elem)))))
                 (when (string-match "${3pjars.dir}." jar)
-                  (setq jar (replace-match (concat basedir "/mcp_3rdParty/lib/jars/") nil nil jar)))
+                  (setq jar (replace-match (concat basedir "mcp_3rdParty/lib/jars/") nil nil jar)))
                 (push jar jars)))))))
     jars))
 
-(defun mcp-jde-setup (basedir classdir &optional jar-list jdk)
+(defvar mcp-ant-home (if (mktop-p) "/opt/ant-1.7.0" "/localdisk/data/matthewk/local/ant-1.7.0"))
+
+(defvar mcp-ant-bin  (concat mcp-ant-home "/bin/ant"))
+
+;; TODO: fish these out of build.xml
+(defvar mcp-ant-jars (mapcar (lambda (j) (concat mk-proj-basedir j))
+                                         '("mcp_3rdParty/lib/jars/build/mcpant.jar"
+                                           "mcp_3rdParty/lib/jars/build/antlr.jar"
+                                           "mcp_3rdParty/lib/jars/optional/axis.jar")))
+
+(defun mcp-jde-setup (basedir workdir stagedir &optional jar-list jdk)
   "Setup JDE for a particular project. Defaults to mcp12 jar-list and preferred jdk."
   (require 'jde)
-  (let ((classpath (mapcar '(lambda (j)
-                              (concat mk-proj-basedir j))
-                           (if (null jar-list) (mcp-jde-jars-from-build-xml basedir) jar-list))))
-    (setq jde-compile-option-directory classdir)
-    (setq jde-compile-option-sourcepath (list (concat mk-proj-basedir "/mcp_core_root/src")))
-    (setq jde-global-classpath (push  classdir classpath))
-    (setq jde-jdk-registry mcp-jde-jdk-registry)
-    (setq jde-jdk (list (if (null jdk) mcp-jde-preferred-jdk jdk)))))
-
-
+  (let ((classpath (if (null jar-list) (mcp-jde-jars-from-build-xml basedir) jar-list))
+        (classdir (concat workdir "classes")))
+    (push (concat workdir "client-compiled/sopi") classpath)
+    (push (concat workdir "client-compiled/omi")  classpath)
+    (push (concat workdir "client-compiled/opi")  classpath)
+    (setq
+     ;; For jde-compile
+     jde-global-classpath         (cons classdir classpath)
+     jde-compiler                 '("javac server" "")
+     jde-compile-option-directory  classdir
+     jde-compile-option-sourcepath (list (concat mk-proj-basedir "mcp_core_root/src"))
+     ;; for jde-ant-build
+     jde-ant-home                  mcp-ant-home
+     jde-ant-program               mcp-ant-bin
+     jde-ant-user-jar-files        mcp-ant-jars
+     jde-ant-args                  (concat "-emacs -Dbuild.compiler.emacs=true -Dno.web.client=true "
+                                          (format "-Dcompile.destination=%s " classdir)
+                                          (format "-Dstaging.dir=%s " stagedir)
+                                          (format "-Dworking.dir=%s " workdir)
+                                          "-Dant.build.javac.target=1.6 -Dant.build.javac.source=1.6 ")
+    jde-ant-buildfile             (concat mk-proj-basedir "mcp_core_root/loadbuild/scripts/build.xml")
+    jde-ant-working-directory     (concat mk-proj-basedir "mcp_core_root/loadbuild/scripts/")
+    jde-ant-read-target           t
+    jde-ant-complete-target       t)))
 
 ;;; --------------------------------------------------------------------
 ;;; MCP Project Auto loader utils
@@ -85,11 +110,11 @@
 ;;; need to stay in this file.
 
 (defvar mcp-proj-dirs (make-hash-table :test 'equal)
-  "Map proj-name to (viewdir workdir) pairs")
+  "Map proj-name to (viewdir workdir stagedir) lists")
 
-(defun mcp-proj-dir-add (name viewdir workdir)
+(defun mcp-proj-dir-add (name viewdir workdir stagedir)
   "Add a project's viewdir/workdir info to our hash"
-  (puthash name (list viewdir workdir) mcp-proj-dirs))
+  (puthash name (list viewdir workdir stagedir) mcp-proj-dirs))
 
 (defun mcp-proj-dir-get (name)
   "Retreive (viewdir workdir) for project `name'"
@@ -105,7 +130,7 @@ set up JDE for the current project"
       (let ((proj-dirs (mcp-proj-dir-get mk-proj-name)))
         (when proj-dirs
           (message "Running generic mcp-jde-setup for %s" mk-proj-name)
-          (mcp-jde-setup (first proj-dirs) (second proj-dirs)))))))
+          (mcp-jde-setup (first proj-dirs) (second proj-dirs) (third proj-dirs)))))))
 
 (defun mcp-proj-generic-shutdown-hook ()
   "Run the user-defined shutdown hook if it exists"
@@ -122,11 +147,12 @@ set up JDE for the current project"
   (let* ((proj-file (concat (homedir) "." name ".ant"))
          (lst (mcp-proj-parse-project-file proj-file)))
     (let ((viewdir (first lst))
-          (workdir (second lst)))
-      (if (and workdir viewdir)
+          (workdir (second lst))
+          (stagedir (third lst)))
+      (if (and workdir viewdir stagedir)
           (let* ((startup-hook-name (concat name "-startup-hook"))
                  (shutdown-hook-name (concat name "-shutdown-hook")))
-            (mcp-proj-dir-add name viewdir workdir)
+            (mcp-proj-dir-add name viewdir workdir stagedir)
             (project-def name
                          `((basedir ,viewdir)
                            (src-patterns ("*.java" "*.jsp"))
@@ -138,13 +164,12 @@ set up JDE for the current project"
                            (startup-hook mcp-proj-generic-startup-hook)
                            (shutdown-hook mcp-proj-generic-shutdown-hook)
                            (vcs git))) ;; TODO: assuming all mcp projects use git for now
-            (message "Created MCP project %s. View is %s, work is %s." name viewdir workdir))
+            (message "Created MCP project %s. View is %s, work is %s, stage is %s" name viewdir workdir stagedir))
         (message "Sorry, can't parse %s MCP project file" proj-file)))))
 
 (defun mcp-proj-parse-project-file (proj-file)
-  "Given a mcp ant project file, return '(viewdir workdir)"
-    (let ((viewdir nil)
-          (workdir nil))
+  "Given a mcp ant project file, return '(viewdir workdir stagedir)"
+    (let (viewdir workdir stagedir)
       (unless (file-readable-p proj-file)
         (error (concat "Unable to read " proj-file ".")))
       (with-temp-buffer
@@ -160,10 +185,12 @@ set up JDE for the current project"
                 (when lst
                   (when (string-equal (car lst) "VIEW_DIR")
                     (setq viewdir (car (cdr lst))))
+                  (when (string-equal (car lst) "STAGE_DIR")
+                    (setq stagedir (car (cdr lst))))
                   (when (string-equal (car lst) "WORK_DIR")
                     (setq workdir (car (cdr lst))))))))
           (forward-line)))
-      (list viewdir workdir)))
+      (list (normalize-dirname viewdir) (normalize-dirname workdir) stagedir)))
 
 (defun mcp-proj-load-all-projects ()
   "Load all projects we have ~/.*.ant files for"
