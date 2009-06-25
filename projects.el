8863df33d4a997753a08ff487e3386640788205c
Matt Keller <mk@littleredbat.net>
2009-06-25 09:40:22 -0400

    Updated mk-project to 1.1

--- projects.el
+++ projects.el
@@ -6,6 +6,7 @@
 
 (global-set-key (kbd "C-c p c") 'project-compile)
 (global-set-key (kbd "C-c p g") 'project-grep)
+(global-set-key (kbd "C-c p a") 'project-ack)
 (global-set-key (kbd "C-c p l") 'project-load)
 (global-set-key (kbd "C-c p u") 'project-unload)
 (global-set-key (kbd "C-c p f") 'project-find-file)
