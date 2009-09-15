02aac4dc3eddeccebc3b9c86f8cb3e91829bf863
Matt Keller <mk@littleredbat.net>
2009-09-14 22:59:28 -0400

    Added mk-swap-windows

--- mk-utils.el
+++ mk-utils.el
@@ -162,6 +162,18 @@
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
+(defun mk-swap-windows (arg)
+  "Swap the buffers shown in two windows."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+
 (defun mk-ido-goto-symbol ()
     "Will update the imenu index and then use ido to select a symbol to navigate to"
     (interactive)
