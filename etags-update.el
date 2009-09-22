fab96fdd45eb2f53fc93a7de2a1dbdda6b1ad3cc
Matt Keller <mk@littleredbat.net>
2009-09-22 14:28:38 -0400

    etags-update.el improvements
    * Customization etu/append-using-font-lock
    * no-prompt-files now a hash
    * Can interactively remove from no-prompt-files
    * Better fn names

--- etags-update.el
+++ etags-update.el
@@ -2,50 +2,63 @@
 ;;
 ;; Minor mode to update the TAGS file when a file is saved
 ;;
-;; TODO:
-;; - Don't offer to add a non-code file to TAGS
 
 (defgroup etags-update nil
   "Minor mode to update the TAGS file when a file is saved"
   :group 'tools)
 
-(defcustom etu/append-file-prompt 'prompt
-  "Add file not already in TAGS to TAGS?
-If `never', do not add the file.
-If `always', add the file.
+(defcustom etu/append-using-font-lock t
+  "If non-nil, will only offer to add a buffer to TAGS if the
+buffer has font-lock-defaults set. This is a weak indicator
+that the buffer represents code, not plain text."
+  :type 'boolean
+  :group 'etags-update)
+
+(defcustom etu/append-file-action 'prompt
+  "What action should be taken when a file not already in TAGS is saved?
+If `nil', do not add the file to TAGS.
+If `add', add the file.
 If `prompt', ask if this file should be added.
-If set to a function, the function should return one of 'always, 'prompt, or 'never."
-  :type '(choice (const always)
+If set to a function, the function should return one of 'add, 'prompt, or 'nil."
+  :type '(choice (const add)
                  (const prompt)
-                 (const never)
+                 (const nil)
                  (function))
   :group 'etags-update)
 
-(defvar etu/files-not-to-prompt-for nil
-  "Once we prompt to add a file to TAGS and the answer is 'no',
-do not ask again for this file.")
+(defvar etu/no-prompt-files (make-hash-table :test 'equal)
+  "A collection of files not to be prompted for in file append situations")
+
+(defun etu/append-prompt-file ()
+  "Remove the curent-buffers's file from the no-prompt-files
+ collection. Then, when the file is saved and
+ `etu/append-file-action' is 'prompt, will prompt to add this
+ file, even if you've answered \"no\" to the prompt before."
+  (interactive)
+  (remhash (buffer-file-name (current-buffer)) etu/no-prompt-files))
 
 (defun etu/tags-file-dir ()
-  "Return full directory of the TAGS file"
+  "Return full directory of the TAGS file (or nil if no tags buffer exists)"
   (when tags-file-name
       (with-current-buffer (get-file-buffer tags-file-name)
         (expand-file-name default-directory))))
 
-(defun etu/file-pattern-in-tags-buffer (buffer pattern)
-  "Search for `pattern' in a 'file' line of a given TAGS
-`buffer'. Return the match or nil"
+(defun etu/file-str-in-tags-buffer (buffer file-str)
+  "Given a file-str which is a relative or absolute filename,
+find a matching file in the given TAGS buffer. Return the
+matching filename or nil."
   (with-current-buffer buffer
     (save-excursion ; useful only for debugging, can remove later
       (goto-char (point-min))
       (let ((match))
         (catch 'loop-exit
-          (while (and (search-forward pattern nil t) (not match))
+          (while (and (search-forward file-str nil t) (not match))
             (beginning-of-line)
             ;; Capture the whole filename on this line. The regex also
             ;; ensures we're on a 'file' line
             (if (re-search-forward "^\\(.*\\),[0-9]+$" nil t)
                 (let ((file-in-tags (buffer-substring (match-beginning 1) (match-end 1))))
-                  (when (string= pattern file-in-tags)
+                  (when (string= file-str file-in-tags)
                     (setq match file-in-tags)))))
           (throw 'loop-exit nil))
         match))))
@@ -58,24 +71,24 @@ the match or nil."
   (let* ((tags-buffer (get-file-buffer tags-file-name))
          (tags-dir    (etu/tags-file-dir))
          (file-rel    (substring file (length tags-dir))))
-    (or (etu/file-pattern-in-tags-buffer tags-buffer file)
+    (or (etu/file-str-in-tags-buffer tags-buffer file)
         (and (string= file (concat tags-dir file-rel)) ; ensure file-rel is in tags-dir
-             (etu/file-pattern-in-tags-buffer tags-buffer file-rel)))))
+             (etu/file-str-in-tags-buffer tags-buffer file-rel)))))
 
-(defun etu/test-file-pattern-in-tags-buffer ()
-  "Testing utu/file-pattern-in-tags-buffer"
+(defun etu/test-file-str-in-tags-buffer ()
+  "Testing utu/file-str-in-tags-buffer"
   (with-temp-buffer
     (insert "junkline\n")
     (insert "/home/mk/foo,10\n")
     (insert "bar,10\n")
     (insert "/home/mk/abcdefg,10\n")
-    (assert (string= "/home/mk/foo" (etu/file-pattern-in-tags-buffer (current-buffer) "/home/mk/foo")))
-    (assert (string= "bar" (etu/file-pattern-in-tags-buffer (current-buffer) "bar")))
-    (assert (string= "/home/mk/abcdefg" (etu/file-pattern-in-tags-buffer (current-buffer) "/home/mk/abcdefg")))
-    (assert (null (etu/file-pattern-in-tags-buffer (current-buffer) "/home/mk/abc")))
-    (assert (null (etu/file-pattern-in-tags-buffer (current-buffer) "bcd")))
-    (assert (null (etu/file-pattern-in-tags-buffer (current-buffer) "mk/abcdefg")))
-    (assert (null (etu/file-pattern-in-tags-buffer (current-buffer) "junkline")))))
+    (assert (string= "/home/mk/foo" (etu/file-str-in-tags-buffer (current-buffer) "/home/mk/foo")))
+    (assert (string= "bar" (etu/file-str-in-tags-buffer (current-buffer) "bar")))
+    (assert (string= "/home/mk/abcdefg" (etu/file-str-in-tags-buffer (current-buffer) "/home/mk/abcdefg")))
+    (assert (null (etu/file-str-in-tags-buffer (current-buffer) "/home/mk/abc")))
+    (assert (null (etu/file-str-in-tags-buffer (current-buffer) "bcd")))
+    (assert (null (etu/file-str-in-tags-buffer (current-buffer) "mk/abcdefg")))
+    (assert (null (etu/file-str-in-tags-buffer (current-buffer) "junkline")))))
 
 (defun etu/update-cb (process event)
   "Callback fn to handle etags-update.pl termination"
@@ -90,11 +103,11 @@ the match or nil."
       (kill-buffer (get-buffer "*etags-update*"))))
    (t (message "Refreshing TAGS file failed. Event was %s. See buffer *etags-update*." event))))
 
-(defun etu/update-for-file ()
+(defun etu/update-tags-for-file ()
   "Update the TAGS file for the file of the current buffer. If
 the file is not already in TAGS, maybe add it."
   (interactive)
-  (catch 'etu/update-for-file
+  (catch 'etu/update-tags-for-file
     (when tags-file-name
       (let ((tags-file-full-name (expand-file-name tags-file-name)))
         (unless (get-file-buffer tags-file-full-name)
@@ -105,27 +118,29 @@ the file is not already in TAGS, maybe add it."
              (cmd               (concat "etags-update.pl " tags-file-name " " file-in-tags))
              (proc-name         "etags-update")
              (default-directory (etu/tags-file-dir)))
-        (if (string= file tags-file-name) (throw 'etu/update-for-file nil))
+        (if (string= file tags-file-name) (throw 'etu/update-tags-for-file nil))
         (unless file-in-tags
           ;; TODO use relative or absolute path? For now, we'll use
           ;; absolute paths. How often do you move your source code OR
           ;; your TAGS file and not completely rebuild TAGS?
           (setq cmd (concat "etags -o " tags-file-name " -a " file))
-          (let ((action etu/append-file-prompt))
-            (when (functionp etu/append-file-prompt)
-              (setq action (funcall etu/append-file-prompt file)))
+          (let ((action etu/append-file-action))
+            (when (functionp etu/append-file-action)
+              (setq action (funcall etu/append-file-action file)))
             (cond
              ((eq action 'prompt)
-              (when (member file etu/files-not-to-prompt-for)
-                (throw 'etu/update-for-file nil))
+              (when (gethash file etu/no-prompt-files)
+                (throw 'etu/update-tags-for-file nil))
+              (when (and etu/append-using-font-lock (null font-lock-defaults))
+                (throw 'etu/update-tags-for-file nil))
               (unless (y-or-n-p (concat "Add " file " to the TAGS file? "))
-                (add-to-list 'etu/files-not-to-prompt-for file)
-                (throw 'etu/update-for-file nil)))
-             ((eq action 'never)
-              (throw 'etu/update-for-file nil))
-             ((eq action 'always)
+                (puthash file 1 etu/no-prompt-files)
+                (throw 'etu/update-tags-for-file nil)))
+             ((eq action 'nil)
+              (throw 'etu/update-tags-for-file nil))
+             ((eq action 'add)
               (message "Ok, adding this file to TAGS"))
-             (t (error "Invalid etu/append-file-prompt action: %s" action)))))
+             (t (error "Invalid etu/append-file-action action: %s" action)))))
         (message "Refreshing TAGS file for %s..." file)
         (start-process-shell-command proc-name "*etags-update*" cmd)
         (set-process-sentinel (get-process proc-name) 'etu/update-cb)))))
@@ -140,8 +155,8 @@ tags-revert-without-query to `t' to avoid tedious prompting."
   :global t
   :lighter " etu"
   (if etags-update-mode
-      (add-hook 'after-save-hook 'etu/update-for-file)
-    (remove-hook 'after-save-hook 'etu/update-for-file)))
+      (add-hook 'after-save-hook 'etu/update-tags-for-file)
+    (remove-hook 'after-save-hook 'etu/update-tags-for-file)))
 
 (provide 'etags-update)
 
