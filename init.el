1c99e6d13dcec614cf30a5720a3044f82ecad18a
Matt Keller <mk@littleredbat.net>
2010-06-12 18:51:16 -0400

    C-x C-x should be exchange-mark-and-point

--- init.el
+++ init.el
@@ -216,7 +216,6 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x C-m") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-x C-X") 'smex-major-mode-commands)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)     ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c C-x C-m") 'execute-extended-command) ;; This is your old M-x.
 ;; (global-set-key (kbd "C-c M-x") 'smex-update-and-run)
