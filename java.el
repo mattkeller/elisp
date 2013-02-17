f777483c46730d3a6addd01e1a5be0be3ad1f384
Matt Keller <mk@littleredbat.net>
2013-02-17 10:28:06 -0500

    Removed jdee

--- java.el
+++ java.el
@@ -2,16 +2,12 @@
 
 (add-to-list 'load-path (concat dotfiles-dir "lib/elib-1.0"))
 (add-to-list 'load-path (concat dotfiles-dir "lib/cedet-1.0pre6/common/"))
-(add-to-list 'load-path (concat dotfiles-dir "lib/jdee-2.4.0/lisp"))
 
 (setq semantic-load-turn-useful-things-on t
       semanticdb-default-save-directory   "~/.semantic.cache"
       semanticdb-persistent-path          nil)
 
-;; Load JDE (and prereqs, like cedet) when we first load a java file,
-;; not on startup
-(autoload 'jde-mode "jde" "JDE mode." t)
-(add-to-list 'auto-mode-alist '("\\.java$" . jde-mode))
+(add-to-list 'auto-mode-alist '("\\.java$" . java-mode))
 
 (when (getenv "JAVA_HOME")
   (let* ((path (getenv "JAVA_HOME"))
@@ -19,18 +15,13 @@
     (setq jde-jdk-registry `((,version . ,(getenv "JAVA_HOME")))
           jde-jdk           version)))
 
-(add-hook 'jde-mode-hook '(lambda () 
-                            (require 'jde-ant)
-                            (message "jde-mode time")
-                            (local-set-key [f5] 'jde-compile)))
-
-
 (add-hook 'java-mode-hook '(lambda ()
+                             (coding-hook)
                              (c-set-style "java")
                              (setq c-basic-offset 3)
                              (setq-default indent-tabs-mode nil
                                            tab-width 4)
-                             (linum-mode)
+                             (subword-mode)
                              (mk-maybe-hide-imports)
                              (local-set-key (kbd "C-.") 'javadoc-lookup)))
 
