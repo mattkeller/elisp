cee35dffa819c2dd16da5816ca5940d5821317f0
Matt Keller <matt@zensight.co>
2015-11-01 20:18:58 -0500

    Remove useless term-toggle-mode

--- basic.el
+++ basic.el
@@ -231,11 +231,4 @@
     (shell buffer)
     buffer))
 
-(defun term-toggle-mode ()
-  (interactive)
-  (if (term-in-line-mode) 
-      (term-char-mode)
-      (term-line-mode)))
-(define-key term-mode-map "\C-c\C-t" 'term-toggle-mode)
-
 (provide 'basic)
